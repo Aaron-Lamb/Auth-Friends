fe6ceba8e52ab445079fd0b64da03919f0972b5f
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-07-10 16:06:41 -0700

    Created the PrivateRoute and refactored my login to a class component

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,41 +1,50 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 
- const Login = () => {
-     const [credentials, setCredentials] = useState({
-         username: '',
-         password: ''
-     })
+ class Login extends React.Component {
+         state = {
+            credentials: {
+                username: '',
+                password: ''
+            }
+         }
 
-     const handleChanges = event => {
-         setCredentials({
-             ...credentials,
-             [event.target.name]: event.target.value
+      handleChanges = event => {
+         this.setState({
+             credentials: {
+                ...this.state.credentials,
+                [event.target.name]: event.target.value
+             }
          })
      }
 
-     const handleSubmit = event => {
+      handleSubmit = event => {
          event.preventDefault();
-         axios.post('http://localhost:5000/api/login', credentials)
+         axios.post('http://localhost:5000/api/login', this.state.credentials)
          .then(response => {
              console.log(response)
              localStorage.setItem('token', response.data.payload)
+             this.props.history.push('/friends')
          })
          .catch(error => console.log(error))
-         setCredentials({
-             username: '',
-             password: ''
+         this.setState({
+             credentials: {
+                 username: '',
+                 password: ''
+             }
          })
      }
-    return(
-        <form onSubmit={handleSubmit}>
-            <label htmlFor='username'>Username: </label>
-            <input name='username' id='username' type='text' value={credentials.username} onChange={handleChanges}/>
-            <label htmlFor='password'>Password: </label>
-            <input name='password' id='password' type='password' value={credentials.password} onChange={handleChanges}/>
-            <button type='submit'>Log in</button>
-        </form>
-    )
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor='username'>Username: </label>
+                <input name='username' id='username' type='text' value={this.state.credentials.username} onChange={this.handleChanges}/>
+                <label htmlFor='password'>Password: </label>
+                <input name='password' id='password' type='password' value={this.state.credentials.password} onChange={this.handleChanges}/>
+                <button type='submit'>Log in</button>
+            </form>
+        )
+    }
 }
 
 export default Login;
